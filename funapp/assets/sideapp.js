c810871fbca50f88db95d036139349f031843608
Yoshiro00 <98204934+Yoshiro00@users.noreply.github.com>
2023-02-27 16:26:41 +0800

    Debug0.1

--- funapp/assets/sideapp.js
+++ funapp/assets/sideapp.js
@@ -1,9 +1,11 @@
 // Fun App Software
 
+const nameData = JSON.parse('{"name":"", "age":, "address":""}');
+
 function SubmitName(){
+    let nameInput = document.getElementById("nameInput").value;
     const chatList = document.getElementById("outputs");
     const messageItem = document.createElement("li");
-    let nameInput = document.getElementById("nameInput").value;
     jsonString = JSON.stringify(nameInput);
 
     messageItem.classList.add("name-output");
@@ -15,4 +17,28 @@ function SubmitName(){
     }
 
     document.getElementById("nameInput").value = "";
+
+    MainEvents();
+}
+
+
+function MainEvents(){
+    fetch('datasets.json')
+        .then(response => response.json())
+        .then (data => {
+            const datasets = data.Datasets;
+
+            return datasets[0];
+        })
+}
+
+
+let age1 = document.getElementById('age1').innerHTML;
+let address1 = document.getElementById('address1').innerHTML;
+
+function ButtonTap(){
+    MainEvents().then(() => {
+        const nameData = data.Username;
+        document.getElementById('name1').textContent = nameData;
+    });
 }
