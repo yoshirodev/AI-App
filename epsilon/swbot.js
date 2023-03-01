ca091f5364f40904722338096880e74a1d904901
Yoshiro00 <98204934+Yoshiro00@users.noreply.github.com>
2023-03-01 22:07:32 +0800

    Upgrades111

--- epsilon/swbot.js
+++ epsilon/swbot.js
@@ -1,3 +1,15 @@
+async function sendUserMessage() {
+    const userMessage = document.getElementById("messageBot").value;
+    if (userMessage.trim() === "") {
+        return;
+    }
+
+    sendQuery();
+    ChangeRandomExpression();
+    document.getElementById("messageBot").value = "";
+}
+
+
 function sendQuery() {
     const userInput = document.getElementById("messageBot").value;
     fetch("https://api.carterapi.com/v0/chat", {
@@ -16,5 +28,14 @@ function sendQuery() {
         .then((data) => {
             document.getElementById("chatList").innerHTML = data.output.text;
     });
+}
+
+function ChangeRandomExpression(){
+    let mascotElement = document.querySelector(".Mascot");
+    let images = ["img/Mascot/e1.png", "img/Mascot/e2.png", "img/Mascot/e3.png", "img/Mascot/e4.png"];
+    let randomIndex = Math.floor(Math.random() * images.length);
+    let randomImage = images[randomIndex];
+    mascotElement.style.backgroundImage = `url(${randomImage})`;
+    mascotElement.classList.remove('changeElem');
 }
   
